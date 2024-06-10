0bc95f3089029120ed2e402d83ce595f9b762e0e
HANAN ALHADE <23041786@student.ltuc.com>
2024-06-10 19:58:56 +0300

    edit 2

--- QuizLab/StartQuiz.cs
+++ QuizLab/StartQuiz.cs
@@ -15,15 +15,16 @@ namespace QuizLab
          {
             "Which of the following is considered an input device?\r\r\nA) Monitor\r\nB) Printer\r\nC) Keyboard\r\nD) Speaker",
             "Which of the following is not an example of an operating system?\r\nA) Windows\r\nB) Linux\r\nC) Microsoft Office\r\nD) macOS",
-            "Which of the following is a non-volatile storage device?\r\nA) RAM\r\nB) SSD\r\nC) Cache memory\r\nD) Register",
-            "What does CPU stand for?\r\nA) Central Processing Unit \r\n B) Computer Processing Unit\r\nC) Central Power Unit\r\nD) Computer Power Unit\r\n",
-            "What is the primary function of a GPU in a computer?\r\nA) Processing input/output operations\r\nB) Handling network communication\r\nC) Rendering graphics\r\nD) Managing storage operations \r\n",
-            "Which type of network connects devices within a limited area, like a home, office, or school?\r\nA) LAN (Local Area Network)\r\nB) WAN (Wide Area Network)\r\nC) MAN (Metropolitan Area Network)\r\nD) PAN (Personal Area Network)\r\n",
-            "What protocol is commonly used for sending and receiving emails?\r\nA) FTP (File Transfer Protocol)\r\nB) SMTP (Simple Mail Transfer Protocol)\r\nC) HTTP (Hypertext Transfer Protocol)\r\nD) UDP (User Datagram Protocol)\r\n",
-            "What does HTML stand for?\r\nA) Hyper Transfer Markup Language\r\nB) Hyperlink Text Markup Language\r\nC) Hypertext Markup Language\r\nD) Hyper Text Manipulation Language\r\n",
-            "What is the purpose of a firewall in network security?\r\n A) To filter and control network traffic\r\nB) To boost internet speed\r\nC) To manage file storage\r\nD) To encrypt data transmissions\r\n",
-            "Which programming language is commonly used for developing web applications?\r\n A) Java\r\nB) C++\r\nC) Python\r\nD) JavaScript\r\n"
-        };
+"Which of the following is a non-volatile storage device?\r\nA) RAM\r\nB) SSD\r\nC) Cache memory\r\nD) Register",
+"What does CPU stand for?\r\nA) Central Processing Unit \r\nB) Computer Processing Unit\r\nC) Central Power Unit\r\nD) Computer Power Unit\r\n",
+"What is the primary function of a GPU in a computer?\r\nA) Processing input/output operations\r\nB) Handling network communication\r\nC) Rendering graphics\r\nD) Managing storage operations \r\n",
+"Which type of network connects devices within a limited area, like a home, office, or school?\r\nA) LAN (Local Area Network)\r\nB) WAN (Wide Area Network)\r\nC) MAN (Metropolitan Area Network)\r\nD) PAN (Personal Area Network)\r\n",
+"What protocol is commonly used for sending and receiving emails?\r\nA) FTP (File Transfer Protocol)\r\nB) SMTP (Simple Mail Transfer Protocol)\r\nC) HTTP (Hypertext Transfer Protocol)\r\nD) UDP (User Datagram Protocol)\r\n",
+"What does HTML stand for?\r\nA) Hyper Transfer Markup Language\r\nB) Hyperlink Text Markup Language\r\nC) Hypertext Markup Language\r\nD) Hyper Text Manipulation Language\r\n",
+"What is the purpose of a firewall in network security?\r\n A) To filter and control network traffic\r\nB) To boost internet speed\r\nC) To manage file storage\r\nD) To encrypt data transmissions\r\n",
+"Which programming language is commonly used for developing web applications?\r\n A) Java\r\nB) C++\r\nC) Python\r\nD) JavaScript\r\n",
+
+ };
 
             string[] answers =
             {
